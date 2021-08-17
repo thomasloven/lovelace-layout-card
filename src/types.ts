c980c5edb7997e44d2926b227c78b5a5736a8362
Elahd Bar-Shai <elahd@users.noreply.github.com>
2021-08-17 14:40:43 +0000

    Fixed linting issues. Added .prettierrc file for consistent future PRs.

--- src/types.ts
+++ src/types.ts
@@ -1,63 +1,63 @@
 export interface LovelaceCard extends HTMLElement {
-	hass: any;
-	editMode?: boolean;
-	setConfig(config: any): void;
-	getCardSize?(): Promise<number> | number;
+  hass: any;
+  editMode?: boolean;
+  setConfig(config: any): void;
+  getCardSize?(): Promise<number> | number;
 }
 
 export interface CardConfig {
-	type: string;
-	view_layout?: {
-		show?:
-			| 'always'
-			| 'never'
-			| {
-					mediaquery?: string;
-					sidebar?: string;
-			  };
-		column?: number;
-	};
+  type: string;
+  view_layout?: {
+    show?:
+      | "always"
+      | "never"
+      | {
+          mediaquery?: string;
+          sidebar?: string;
+        };
+    column?: number;
+  };
 }
 
 export interface CardConfigGroup {
-	card: LovelaceCard;
-	config: CardConfig;
-	index: number;
-	show?: boolean;
+  card: LovelaceCard;
+  config: CardConfig;
+  index: number;
+  show?: boolean;
 }
 
 export interface ViewConfig {
-	title?: string;
-	type?: string;
-	cards?: Array<CardConfig>;
-	layout?: {};
-	view_layout?: {};
+  title?: string;
+  type?: string;
+  cards?: Array<CardConfig>;
+  layout?: {};
+  view_layout?: {};
 }
 
 export interface ColumnViewConfig extends ViewConfig {
-	layout?: {
-		width?: number;
-		column_widths: string;
-		max_width?: number;
-		max_cols?: number;
-		min_height?: number;
-		rtl?: boolean;
-	};
+  layout?: {
+    width?: number;
+    column_widths: string;
+    max_width?: number;
+    max_cols?: number;
+    min_height?: number;
+    rtl?: boolean;
+  };
 }
 
 export interface GridViewConfig extends ViewConfig {
-	layout?: {
-		mediaquery?: Array<Record<string, any>>;
-	};
-	style?: {
-		margin: string;
-	};
+  layout?: {
+    mediaquery?: Array<Record<string, any>>;
+  };
+  style?: {
+    margin: string;
+  };
 }
 
 export interface LayoutCardConfig {
-	cards?: Array<CardConfig>;
-	entities?: Array<CardConfig>;
-	layout_type?: string;
-	layout?: any;
-	layout_options?: any; // legacy
+  cards?: Array<CardConfig>;
+  entities?: Array<CardConfig>;
+  layout_type?: string;
+  layout?: any;
+  layout_options?: any; // legacy
 }
