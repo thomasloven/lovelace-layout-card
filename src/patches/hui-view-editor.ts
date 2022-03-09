b599eef9794b0dbcbbea548da7861877620c304b
Thomas Lovén <thomasloven@gmail.com>
2022-03-09 14:00:39 +0000

    Update view-editor patch. Fix #185

--- src/patches/hui-view-editor.ts
+++ src/patches/hui-view-editor.ts
@@ -4,48 +4,38 @@ const LAYOUT_TYPES = ["masonry", "horizontal", "vertical", "grid"];
 customElements.whenDefined("hui-view-editor").then(() => {
   const HuiViewEditor = customElements.get("hui-view-editor");
 
-  loadHaYamlEditor();
-
   const firstUpdated = HuiViewEditor.prototype.firstUpdated;
   HuiViewEditor.prototype.firstUpdated = function () {
     firstUpdated?.bind(this)();
 
-    const listBox = this.shadowRoot.querySelector("mwc-select");
-    if (!listBox || listBox.layoutCardPatch) return;
+    this._oldSchema = this._schema;
+    this._schema = (localize) => {
+      const retval = this._oldSchema(localize);
+      const typeSelector = retval[retval.length - 1];
+      typeSelector.selector.select.options.push({
+        value: "custom:masonry-layout",
+        label: "Masonry (layout-card)",
+      });
+      typeSelector.selector.select.options.push({
+        value: "custom:horizontal-layout",
+        label: "Horizontal (layout-card)",
+      });
+      typeSelector.selector.select.options.push({
+        value: "custom:vertical-layout",
+        label: "Vertical (layout-card)",
+      });
+      typeSelector.selector.select.options.push({
+        value: "custom:grid-layout",
+        label: "Grid (layout-card)",
+      });
+      console.log(retval, typeSelector);
+      retval.push({
+        name: "layout",
+        selector: { object: {} },
+      });
+      return retval;
+    };
 
-    LAYOUT_TYPES.forEach((type) => {
-      const el = document.createElement("mwc-list-item");
-      (el as any).value = `custom:${type}-layout`;
-      el.innerHTML = `${type} (layout-card)`;
-      listBox.appendChild(el);
-    });
-    listBox.layoutCardPatched = true;
-
-    const layoutEditor = document.createElement("ha-yaml-editor");
-    (layoutEditor as any).label = "Layout options";
-    (layoutEditor as any).defaultValue = this._config.layout ?? "";
-    layoutEditor.addEventListener("value-changed", (ev: CustomEvent) => {
-      ev.stopPropagation();
-      const newConfig = { ...this._config };
-      newConfig.layout = ev.detail.value;
-      this.dispatchEvent(
-        new CustomEvent("view-config-changed", {
-          detail: { config: newConfig },
-        })
-      );
-    });
-
-    this.shadowRoot.appendChild(layoutEditor);
-  };
-
-  const updated = HuiViewEditor.prototype.updated;
-  HuiViewEditor.prototype.updated = function (_changedProperties) {
-    updated?.bind(this)(_changedProperties);
-
-    if (_changedProperties.has("_config")) {
-      const layoutEditor = this.shadowRoot.querySelector("ha-yaml-editor");
-      if (!layoutEditor) return;
-      (layoutEditor as any).defaultValue = this._config.layout;
-    }
+    this.requestUpdate();
   };
 });
